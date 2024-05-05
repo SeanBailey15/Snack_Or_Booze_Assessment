a669d7252f193e7463124cc54af985b87a5c9230
Sean Bailey <seanbailey15@gmail.com>
2024-05-05 12:44:46 -0400

    set up snack or booze after troubleshooting starter code

---
+++ snack_or_booze/src/Api.jsx
@@ -0,0 +1,56 @@
+import axios from "axios";
+
+const BASE_API_URL = "http://localhost:5000";
+
+/* 
+  json-server will give you CRUD endpoints on snacks and drinks.
+  Here we've provided you with a single action to get all drinks.
+
+  You'll need to add to this class as you build features for the app.
+*/
+
+class SnackOrBoozeApi {
+  static async getSnacks() {
+    const result = await axios.get(`${BASE_API_URL}/snacks`);
+    return result.data;
+  }
+
+  static async postSnack(data) {
+    const { id, name, description, recipe, serve } = data;
+    try {
+      const res = await axios.post(`${BASE_API_URL}/snacks`, {
+        id: id,
+        name: name,
+        description: description,
+        recipe: recipe,
+        serve: serve,
+      });
+      return res.data;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  static async getDrinks() {
+    const result = await axios.get(`${BASE_API_URL}/drinks`);
+    return result.data;
+  }
+
+  static async postDrink(data) {
+    const { id, name, description, recipe, serve } = data;
+    try {
+      const res = await axios.post(`${BASE_API_URL}/drinks`, {
+        id: id,
+        name: name,
+        description: description,
+        recipe: recipe,
+        serve: serve,
+      });
+      return res.data;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+export default SnackOrBoozeApi;
--- snack_or_booze/src/App.jsx
+++ snack_or_booze/src/App.jsx
@@ -1,10 +1,50 @@
+import { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-const App = () => {
+import "./App.css";
+import Home from "./Home";
+import SnackOrBoozeApi from "./Api";
+import NavBar from "./NavBar";
+import Menu from "./FoodMenu";
+import Snack from "./FoodItem";
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [snacks, setSnacks] = useState([]);
+
+  useEffect(() => {
+    async function getSnacks() {
+      let snacks = await SnackOrBoozeApi.getSnacks();
+      setSnacks(snacks);
+      setIsLoading(false);
+    }
+    getSnacks();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading &hellip;</p>;
+  }
+
   return (
-    <>
-      <div></div>
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home snacks={snacks} />} />
+            <Route
+              path="/snacks"
+              element={<Menu snacks={snacks} title="Snacks" />}
+            />
+            <Route
+              path="/snacks/:id"
+              element={<Snack items={snacks} cantFind="/snacks" />}
+            />
+            <Route element={<p>Hmmm. I can't seem to find what you want.</p>} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </div>
   );
-};
+}
 
 export default App;
